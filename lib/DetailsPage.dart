1c7e4a73c74952a05db33eb5670f76294cf0212f
KasunHasanga <kasunhasanga96@gmail.com>
2020-11-30 01:52:34 +0530

    Add flutter_html package and remove html attributes in film summary

--- lib/DetailsPage.dart
+++ lib/DetailsPage.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/painting.dart';
 import 'package:tv_show_app/Model/ShowModel.dart';
+import 'package:flutter_html/flutter_html.dart';
 
 class DetailsPage extends StatefulWidget {
   DetailsPage({Key key, this.show}) : super(key: key);
@@ -20,19 +22,23 @@ class _DetailsPageState extends State<DetailsPage> {
 
   getListViewCells() {
     List<Widget> widgets = [];
+    widgets.add(
+      Text(widget.show.name,style: TextStyle(fontWeight: FontWeight.bold,fontSize: 23),),
+    );
     widgets.add(Container(
       child: Image.network(
         widget.show.image.medium.replaceAll('http', 'https'),
+        height: 300,
       ),
     ));
+
     widgets.add(
-      Text(widget.show.name),
+      Text("Rating ${widget.show.rating.average}",style: TextStyle(fontSize: 20,fontWeight: FontWeight.w500),),
     );
-    widgets.add(
-      Text("Rating ${widget.show.rating.average}"),
-    );
-    widgets.add(Text('${widget.show.summary}'));
+    //widgets.add(Text('${widget.show.summary}'));
+    widgets.add(Html(data: widget.show.summary,defaultTextStyle:TextStyle(fontSize: 16) ,));
     return widgets;
+
   }
 
   @override
@@ -42,10 +48,13 @@ class _DetailsPageState extends State<DetailsPage> {
         title: Text(widget.show.name),
       ),
       body: Center(
-        child: ListView.builder(itemBuilder: (BuildContext context, int index) {
-          return widgets[index];
-        },
-        itemCount: widgets.length,),
+        child: Padding(
+          padding: EdgeInsets.fromLTRB(20, 0, 15, 20),
+          child: ListView.builder(itemBuilder: (BuildContext context, int index) {
+            return widgets[index];
+          },
+          itemCount: widgets.length,),
+        ),
       ),
     );
   }
